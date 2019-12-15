38002823c129f7c1d2b92539262b4c63f47e0d73
Fatih Can Kurnaz <fatihcankurnaz@gmail.com>
2019-12-16 01:23:59 +0300

    v2

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ import json
 import numpy as np
 def main():
     image_dicts = {}
+    image_dicts_counted = {}
     id_to_name = {}
     cate = {}
     matrix = np.zeros((1988,54))
@@ -9,12 +10,14 @@ def main():
         data = json.load(json_file)
         for image in data["images"]:
             image_dicts[image['id']] = {}
+            image_dicts_counted[image['id']] = {}
         for anns in data['annotations']:
             if anns['category_id'] not in image_dicts[anns['image_id']]:
                 image_dicts[anns['image_id']][anns['category_id']] = 1
+                image_dicts_counted[anns['image_id']][anns['category_id']] = 1
                 
             else:
-                image_dicts[anns['image_id']][anns['category_id']] += 1
+                image_dicts_counted[anns['image_id']][anns['category_id']] += 1
                 
         for cats in data['categories']:
             id_to_name[cats['id']] = cats['name']
@@ -24,24 +27,28 @@ def main():
         for image in data["images"]:
             
             image_dicts[image['id']] = {}
+            image_dicts_counted[image['id']] = {}
         for anns in data['annotations']:
             if anns['category_id'] not in image_dicts[anns['image_id']]:
                 image_dicts[anns['image_id']][anns['category_id']] = 1
+                image_dicts_counted[anns['image_id']][anns['category_id']] = 1
                 
             else:
-                image_dicts[anns['image_id']][anns['category_id']] += 1
+                image_dicts_counted[anns['image_id']][anns['category_id']] += 1
                 
         
     with open("val.json") as json_file:
         data = json.load(json_file)
         for image in data["images"]:
             image_dicts[image['id']] = {}
+            image_dicts_counted[image['id']] = {}
         for anns in data['annotations']:
             if anns['category_id'] not in image_dicts[anns['image_id']]:
                 image_dicts[anns['image_id']][anns['category_id']] = 1
+                image_dicts_counted[anns['image_id']][anns['category_id']] = 1
   
             else:
-                image_dicts[anns['image_id']][anns['category_id']] += 1
+                image_dicts_counted[anns['image_id']][anns['category_id']] += 1
                 
     
     named_dict = {}
@@ -62,10 +69,16 @@ def main():
             cate[anns] += named_dict[i][anns]
     #probability of one item existing 
     prop_of_one_item = dict(cate)
+    
     for i in cate:
         prop_of_one_item[i] = prop_of_one_item[i]/len(named_dict.keys())
     cov_matrix = np.cov(matrix)
-    print(cov_matrix)
+    #print(cov_matrix)
+    sum = 0
+    for i in prop_of_one_item:
+        sum+= prop_of_one_item[i]
+    print(prop_of_one_item)
+    print(sum)
     
 
                 
